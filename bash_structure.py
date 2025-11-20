1854679cdfb170cb124dabfe39eab65c27335f13
ruanoliveiragf <ruanoliveiragf@gmail.com>
2025-11-20 16:44:33 -0300

    Updating file

--- bash_structure.py
+++ bash_structure.py
@@ -4,6 +4,7 @@ import commands
 def exibir_prompt():
     #podemos usar a write https://docs.python.org/pt-br/3/library/os.html#os.write
     mensagem = "> ".encode('utf-8') #write só escreve em bytes, então precisa pegar em string e transformar para bytes https://docs.python.org/pt-br/3/library/stdtypes.html#str.encode
+    #os.write(1, mensagem)
     os.write(1, mensagem)
 
 def main():
